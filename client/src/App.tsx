bdf9b484b34ae951d7b48c7581006a3bfe2cb7fb
Stefan Koelle <stefan.koelle@gmail.com>
2026-08-09 15:44:08 +0200

    change react component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { DragDropContext, Droppable, Draggable, DropResult } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, Draggable, DropResult } from '@hello-pangea/dnd';
 import { focusTaskApi } from './api';
 import { FocusTask } from './types';
 import TaskCard from './components/TaskCard';
